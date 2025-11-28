686770ef02b97192ecb1f3f87171d289d8d9e72c
amanda-v-yuan <79546289+amanda-v-yuan@users.noreply.github.com>
2025-11-28 16:14:31 -0500

    Update ingredients.js

--- data/ingredients.js
+++ data/ingredients.js
@@ -1,3 +1,4 @@
+// ingredients.js
 export const INGREDIENTS = [
   {id:'tomato', name:'Tomato', color:'#ef4444', rarity:'common'},
   {id:'lettuce', name:'Lettuce', color:'#16a34a', rarity:'common'},
@@ -12,4 +13,17 @@ export const INGREDIENTS = [
   {id:'rice', name:'Rice', color:'#fff', rarity:'common'},
 ];
 
-export const ING_MAP = Object.fromEntries(INGREDIENTS.map(i => [i.id, i]));
+export const BOX_WEIGHTS = { common: 70, uncommon: 20, rare: 8, epic: 2 };
+export const PREMIUM_WEIGHTS = { common: 50, uncommon: 30, rare: 15, epic: 5 };
+export const BOX_COST = 100;
+export const PREMIUM_COST = 200;
+
+export const RECIPES = [
+  {id:'salad', name:'Fresh Salad', requires:{lettuce:1,tomato:1}, points:40},
+  {id:'omelette', name:'Omelette', requires:{egg:2,cheese:1}, points:80},
+  {id:'truffle_pasta', name:'Truffle Pasta', requires:{flour:1,garlic:1,truffle:1}, points:180},
+  {id:'saffron_risotto', name:'Saffron Risotto', requires:{rice:1,saffron:1}, points:220},
+  {id:'gold_dessert', name:'Gold Dessert', requires:{flour:1,egg:1,goldleaf:1}, points:400}
+];
+
+export const ING_MAP = Object.fromEntries(INGREDIENTS.map(i=>[i.id,i]));
