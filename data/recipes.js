2e8a69e5d8096844b67b677ce750b7e6f754aa9c
amanda-v-yuan <79546289+amanda-v-yuan@users.noreply.github.com>
2025-11-28 15:52:11 -0500

    Create recipes.js

---
+++ data/recipes.js
@@ -0,0 +1,7 @@
+export const RECIPES = [
+  {id:'salad', name:'Fresh Salad', requires:{lettuce:1,tomato:1}, points:40},
+  {id:'omelette', name:'Omelette', requires:{egg:2,cheese:1}, points:80},
+  {id:'truffle_pasta', name:'Truffle Pasta', requires:{flour:1,garlic:1,truffle:1}, points:180},
+  {id:'saffron_risotto', name:'Saffron Risotto', requires:{rice:1,saffron:1}, points:220},
+  {id:'gold_dessert', name:'Gold Dessert', requires:{flour:1,egg:1,goldleaf:1}, points:400}
+];
